56b668838ca799ff938c673477b173f34196fcc5
dkrivaa <dannykrivaa@hotmail.com>
2023-09-28 11:21:19 +0300

    Matching preferences of managers and employees regarding appointments

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # This is the main app file
+import streamlit as st
 import engine
 
 engine.read_data()
 
-
+st.download_button('Download example CSV file',
+                   data=csv,
+                   file_name='example.csv',
+                   mime='text/csv')
 
