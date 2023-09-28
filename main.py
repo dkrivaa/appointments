ca8b681e2abfed80d4f927757cc2d3d0f9d13212
dkrivaa <dannykrivaa@hotmail.com>
2023-09-28 11:43:17 +0300

    Matching preferences of managers and employees regarding appointments

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import engine
 engine.read_data()
 
 st.download_button('Download example CSV file',
+                   data='example.csv',
                    file_name='example.csv',
                    mime='text/csv')
 
