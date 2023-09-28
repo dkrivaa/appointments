0800f417fa8a69cb9526b0b3e2f14854456b9ea6
dkrivaa <dannykrivaa@hotmail.com>
2023-09-28 13:08:19 +0300

    Matching preferences of managers and employees regarding appointments

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import streamlit as st
 import engine
 import example
 
-engine.read_data()
-
 
 df = example.example()
-
-
 def convert_df(df):
     return df.to_csv(index=False).encode('utf-8')
 
-
 csv = convert_df(df)
-
 st.download_button('Download example CSV file',
                    data=csv,
                    file_name='example.csv',
                    mime='text/csv')
+
+
+engine.read_data()
+
+
+
+
 
