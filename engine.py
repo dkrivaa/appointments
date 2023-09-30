f0edad8534f2f155df925937e8fcd20b3e2b1e94
dkrivaa <dannykrivaa@hotmail.com>
2023-09-30 12:38:20 +0300

    Matching preferences of managers and employees regarding appointments

--- engine.py
+++ engine.py
@@ -180,7 +180,8 @@ def organize():
     st.markdown('___')
 
     st.subheader('Summary')
-    st.write(f'Number of positions that got one of top wishes: **{pos_count}**')
+    st.write(f'Number of positions that got one of top wishes: **{pos_count}** '
+             f'out of {len(tentative_appoint)} open positions')
     st.write(f'Number of employees that got one of top wishes: **{off_count}** '
              f'(out of **{possible}** that have corresponding wishes with positions)')
 
