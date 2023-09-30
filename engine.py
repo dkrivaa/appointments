adc47125ee3f19286c579b2c1e93594f5e708d15
dkrivaa <dannykrivaa@hotmail.com>
2023-09-30 12:36:38 +0300

    Matching preferences of managers and employees regarding appointments

--- engine.py
+++ engine.py
@@ -25,11 +25,11 @@ def organize():
     df = st.session_state.df
 
     # Adding 'o' or 'p' to preference to imply if referring to officer or position
-    df['pref_1'] = df.apply(lambda row: 'employee' + str(row['pref_1']) if row['position_or_officer'] == 'p'
+    df['pref_1'] = df.apply(lambda row: 'officer' + str(row['pref_1']) if row['position_or_officer'] == 'p'
                             else 'position' + str(row['pref_1']), axis=1)
-    df['pref_2'] = df.apply(lambda row: 'employee' + str(row['pref_2']) if row['position_or_officer'] == 'p'
+    df['pref_2'] = df.apply(lambda row: 'officer' + str(row['pref_2']) if row['position_or_officer'] == 'p'
                             else 'position' + str(row['pref_2']), axis=1)
-    df['pref_3'] = df.apply(lambda row: 'employee' + str(row['pref_3']) if row['position_or_officer'] == 'p'
+    df['pref_3'] = df.apply(lambda row: 'officer' + str(row['pref_3']) if row['position_or_officer'] == 'p'
                             else 'position' + str(row['pref_3']), axis=1)
 
     # making preferences into list and dropping the individual columns
