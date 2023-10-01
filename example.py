0afed696b762300f28e46826710717e169820d33
dkrivaa <dannykrivaa@hotmail.com>
2023-10-01 13:14:11 +0300

    Matching preferences of managers and employees regarding appointments

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Example data
 def example():
-    pos_off = ['p', 'p', 'p', 'p', 'p', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+    pos_emp = ['p', 'p', 'p', 'p', 'p', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e']
     id = ['position1', 'position2', 'position3', 'position4', 'position5',
           'employee1', 'employee2', 'employee3', 'employee4', 'employee5',
           'employee6', 'employee7', 'employee8', 'employee9', 'employee10',
@@ -12,7 +12,7 @@ def example():
     pref_1 = [2, 2, 2, 2, 12, 5, 4, 5, 1, 1, 2, 5, 4, 2, 2, 4, 2, 5, 4, 4, 4, 1, 2, 3, 4]
     pref_2 = [19, 12, 14, 7, 3, 1, 1, 2, 3, 5, 1, 2, 1, 3, 4, 1, 1, 3, 2, 1, 3, 2, 3, 1, 5]
     pref_3 = [5, 1, 11, 18, 19, 2, 3, 3, 2, 2, 4, 3, 5, 1, 3, 2, 3, 1, 1, 5, 5, 3, 1, 4, 2]
-    data = {'position_or_employee': pos_off, 'id': id, 'pref_1': pref_1,
+    data = {'position_or_employee': pos_emp, 'id': id, 'pref_1': pref_1,
             'pref_2': pref_2, 'pref_3': pref_3}
 
     df = pd.DataFrame(data)
