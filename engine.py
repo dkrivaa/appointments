e88ea1f63349ca34d07caa538190900c5cd3fedd
dkrivaa <dannykrivaa@hotmail.com>
2023-10-01 12:38:13 +0300

    Matching preferences of managers and employees regarding appointments

--- engine.py
+++ engine.py
@@ -113,6 +113,7 @@ def organize():
             elif len(taken_match) > 0:
                 print(f'{employee} is tentatively appointed already')
                 st.write(taken_match[0][0], employee)
+                st.write(position_dict[position].index[employee])
 
 
                 def points(position, employee):
