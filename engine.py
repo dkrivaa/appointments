d39aa4f17228de3f80c4c99a72ad7cc65a7d7e8c
dkrivaa <dannykrivaa@hotmail.com>
2023-10-01 17:02:38 +0300

    Matching preferences of managers and employees regarding appointments

--- engine.py
+++ engine.py
@@ -112,7 +112,7 @@ def organize():
             points = position_points + employee_points
             return points
 
-        st.write(f'Dealing with position {position}')
+        st.write(f'Dealing with {position}')
 
         # Sorting position pref list by combined points of prefs
         best_list = []
