9be5ea3212c8ed213ae74371e6f7d056b78a1bbe
dkrivaa <dannykrivaa@hotmail.com>
2023-10-04 17:09:46 +0300

    Matching preferences of managers and employees regarding appointments

--- engine2.py
+++ engine2.py
@@ -264,10 +264,10 @@ def organize():
     # Making csv file of results to download
     pos = [sublist[0] for sublist in tentative_appoint]
     emp = [sublist[1] for sublist in tentative_appoint]
-    df_results = pd.DataFrame({'position': pos, 'employee': emp})
+    # df_results = pd.DataFrame({'position': pos, 'employee': emp})
     # Code for the user entered positions and candidates (doesn't work with hebrew)
-    # df_results = pd.DataFrame({f'{upperhand}': real_position_list,
-    #                            f'{lowerhand}': real_candidate_list})
+    df_results = pd.DataFrame({f'{upperhand}': real_position_list,
+                               f'{lowerhand}': real_candidate_list})
 
     def convert_df(df_any):
         return df_any.to_csv(index=False).encode('utf-8')
