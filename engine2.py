6d6044795050744dad9f06506476c50829ae8763
dkrivaa <dannykrivaa@hotmail.com>
2023-10-04 09:39:46 +0300

    Matching preferences of managers and employees regarding appointments

--- engine2.py
+++ engine2.py
@@ -59,7 +59,7 @@ def organize():
     df.rename(columns={df.columns[1]: 'id'}, inplace=True)
 
     #################
-    # df['work_id'] = df['id']
+    df['work_id'] = df['id']
     df['work-id'] = df.apply(lambda row: f'{upperhand}' + str(row['work_id']) if row[first_column] == 'p'
     else f'{lowerhand}' + str(row['work_id']), axis=1)
 
