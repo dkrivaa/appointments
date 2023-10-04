8c6e18789e6a74016ce9b1ee65341c453149b1a7
dkrivaa <dannykrivaa@hotmail.com>
2023-10-04 19:01:50 +0300

    Matching preferences of managers and employees regarding appointments

--- enter_data.py
+++ enter_data.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 
 def enter_data():
-    n_prefs_pos = st.number_input('Managers - Number of choices',
-                                   min_value=1, max_value=10, value=3)
-    n_prefs_emp = st.number_input('Candidates - Number of choices',
-                                   min_value=1, max_value=10, value=3)
-    continue1 = st.button('Press to continue')
+    with st.form:
+        n_prefs_pos = st.number_input('Managers - Number of choices',
+                                      min_value=1, max_value=10, value=3)
+        n_prefs_emp = st.number_input('Candidates - Number of choices',
+                                      min_value=1, max_value=10, value=3)
+        submit1 = st.form_submit_button('Press to continue')
 
-    if continue1:
+    if submit1:
         with st.form('Enter your data'):
             data_type = st.radio('Choose type of data', ('open position', 'candidate'))
             identity = st.text_input('Enter ID of position or candidate')
