92f6b18eb09b9cd0e5c47b33c68d451f219ca43f
dkrivaa <dannykrivaa@hotmail.com>
2023-10-04 10:28:17 +0300

    Matching preferences of managers and employees regarding appointments

--- engine2.py
+++ engine2.py
@@ -60,9 +60,9 @@ def organize():
 
     #################
     st.write(df)
-
-    df['work-id'] = df.apply(lambda row: f'{upperhand}' + df.index.astype(str) if row[first_column] == 'p'
-                             else f'{lowerhand}' + df.index.astype(str), axis=1)
+    df['work_id'] = f'{upperhand}' + df.index.astype(str)
+    # df['work_id'] = df.apply(lambda row: f'{upperhand}' + df.index.astype(str) if row[first_column] == 'p'
+    #                          else f'{lowerhand}' + df.index.astype(str), axis=1)
 
     st.write(df)
     #################
