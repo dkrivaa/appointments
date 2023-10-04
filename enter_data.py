192408f4a0c6cecd98cbba55dd09e285f5cfb92c
dkrivaa <dannykrivaa@hotmail.com>
2023-10-04 18:13:03 +0300

    Matching preferences of managers and employees regarding appointments

--- enter_data.py
+++ enter_data.py
@@ -6,4 +6,4 @@ def enter_data():
     with st.form('Enter your data'):
         data_type = st.radio('Choose type of data', ('open position', 'candidate'))
 
-        submit = st.form_submit_button
+        submit = st.form_submit_button()
