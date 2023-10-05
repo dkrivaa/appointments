b77e78fe61c7122a5eec540db0afa19826867ab6
dkrivaa <dannykrivaa@hotmail.com>
2023-10-05 07:00:27 +0300

    Matching preferences of managers and employees regarding appointments

--- enter_data.py
+++ enter_data.py
@@ -10,18 +10,19 @@ def enter_data():
         n_prefs_emp = st.number_input('Candidates - Max umber of choices',
                                       min_value=1, max_value=10, value=3)
         submit1 = st.form_submit_button('Press to continue', type='primary')
-
-        if submit1:
-            # Making empty dataframe
-            biggest = max[n_prefs_pos, n_prefs_emp]
-            st.write(biggest)
-
-            df_data = {'col1': [], 'col2': [],  }
-
-            with st.form(key='form2'):
-                data_type = st.radio('Choose type of data', ('open position', 'candidate'))
-                identity = st.text_input('Enter ID of position or candidate')
-
-
-                submit = st.form_submit_button()
+        biggest = max[n_prefs_pos, n_prefs_emp]
+        st.write(biggest)
+        # if submit1:
+        #     # Making empty dataframe
+        #     biggest = max[n_prefs_pos, n_prefs_emp]
+        #     st.write(biggest)
+        #
+        #     df_data = {'col1': [], 'col2': [],  }
+        #
+        #     with st.form(key='form2'):
+        #         data_type = st.radio('Choose type of data', ('open position', 'candidate'))
+        #         identity = st.text_input('Enter ID of position or candidate')
+        #
+        #
+        #         submit = st.form_submit_button()
 
