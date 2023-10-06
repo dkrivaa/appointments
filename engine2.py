68d9ddc8e89ad0dd7e9064bfce86150494b7c956
dkrivaa <dannykrivaa@hotmail.com>
2023-10-06 08:25:30 +0300

    Matching preferences of managers and employees regarding appointments

--- engine2.py
+++ engine2.py
@@ -59,9 +59,9 @@ def read_data():
                 if continue_button:
                     organize()
 
-        except KeyError as e:
-            st.write('Your file is not compatible. Download CSV example file '
-                     '(see bottom of page)')
+        # except KeyError as e:
+        #     st.write('Your file is not compatible. Download CSV example file '
+        #              '(see bottom of page)')
 
 
 def organize():
