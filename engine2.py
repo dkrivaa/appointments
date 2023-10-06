c4e97fef15b04a1b630d13eff4fdb280421c4bf0
dkrivaa <dannykrivaa@hotmail.com>
2023-10-06 11:16:48 +0300

    Matching preferences of managers and employees regarding appointments

--- engine2.py
+++ engine2.py
@@ -38,7 +38,7 @@ def start():
     if lowerhand not in st.session_state:
         st.session_state.lowerhand = lowerhand
 
-    st.markdown(''':rainbow[___]''')
+    st.divider(''':rainbow[]''')
     read_data()
 
 
